4b149331ec8682d0e3f4deb174ff47525ce994e2
Arthur Vinícius <40926020+arthurvinice@users.noreply.github.com>
2024-04-13 00:41:36 -0300

    Update vector sizes

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -27,7 +27,7 @@ void selectionSort(vector<int> &v){
 
 int main() {
     //Defining vector sizes for testing
-    vector<int> sizes = {1000, 10000, 25000, 50000, 75000, 100000}; //Sizes for testing
+    vector<int> sizes = {10000, 25000, 50000, 75000, 100000, 200000, 500000}; //Sizes for testing
 
     for (int size : sizes) {
         //Generating a random vector
